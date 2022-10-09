2d5204f9ebbdac9e81037a79a730a5bd1624bea9
Lucas Bastos <lucasfbastos1987@gmail.com>
2022-10-09 17:35:44 -0300

    Exemplo Construtor

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using ExemploExplorando.Models;
+
+ Pessoa p1 = new Pessoa("Lucas", "Bastos");
+
+Pessoa p2 = new Pessoa(nome: "Daiana", sobrenome: "Vieira Lopes");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Lucas";
+// p1.Sobrenome = "Bastos";
+// p1.Idade = 34;
+// p1.Apresentar();
